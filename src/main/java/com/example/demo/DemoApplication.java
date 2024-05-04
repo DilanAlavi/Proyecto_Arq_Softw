fadcb64ef82b6aacc7b4f8ea2d76792dc18e9b0e
DILAN ALVARO ALAVI GUTIERREZ <dilanalavi11@gmail.com>
2024-05-03 21:38:08 -0400

    SWAGGER, CONFIGURADO CORRECTAMENTE

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,35 @@
 package com.example.demo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+    info = @Info(
+        title = "My API", 
+        version = "1.0", 
+        description = "This is a detailed description of my API. It provides information about various endpoints.",
+        termsOfService = "http://example.com/terms/",
+        contact = @Contact(
+            name = "Support Team",
+            url = "http://example.com/contact",
+            email = "support@example.com"
+        ),
+        license = @License(
+            name = "Apache 2.0",
+            url = "http://www.apache.org/licenses/LICENSE-2.0.html"
+        )
+    ),
+    servers = @Server(url = "http://localhost:8080", description = "Local server")
+)
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
